4f7ed0b046373c110135aee567d45a3d6068b65c
tarafdar16 <imrantarafdar16@gmail.com>
2024-01-25 14:39:37 +0600

    done

--- src/pages/Home/CallUs/CallUs.jsx
+++ src/pages/Home/CallUs/CallUs.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const CallUs = () => {
   return (
-    <div className="bg-black text-center p-24">
-      <h3 className="text-white text-5xl">Call Us: +8801760575664</h3>
+    <div className="bg-black text-center md:p-24 p-10">
+      <h3 className="text-white md:text-5xl text-2xl">Call Us: +8801760575664</h3>
     </div>
   );
 };
